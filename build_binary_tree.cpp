243e95b4e341368a9ee29e902d6020bea93bb1c4
ayush8006 <ayushagrawal8006@gmail.com>
2024-06-21 02:31:07 +0530

    first commit

---
+++ build_binary_tree.cpp
@@ -0,0 +1,13 @@
+//1. by level order traversal 
+
+
+
+//2. recursively create a tree
+//create root
+//root->left->recursive
+//root->right->recursive
+
+//assume if i/p ==-1 then null and !=-1 valid data
+
+
+
